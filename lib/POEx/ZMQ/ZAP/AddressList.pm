9d8d5357cb61c307110535a45a79709dbf1ac5d5
Jon Portnoy <avenj@cobaltirc.org>
2014-12-07 21:46:27 -0500

    POD, use TypedArrays for mask lists

--- lib/POEx/ZMQ/ZAP/AddressList.pm
+++ lib/POEx/ZMQ/ZAP/AddressList.pm
@@ -9,9 +9,9 @@ use Types::Standard       -types;
 
 sub _match {
   my ($mask, $addr) = @_;
-  $addr = lc $addr;
+  $addr = uc $addr;
 
-  my $quoted = quotemeta lc $mask;
+  my $quoted = quotemeta uc $mask;
   $quoted =~ s/\\\*/[\x01-\xFF]{0,}/g;
   $quoted =~ s/\\\?/[\x01-\xFF]{1,1}/g;
 
@@ -24,27 +24,21 @@ use Moo; use MooX::late;
 has masks => (
   lazy      => 1,
   is        => 'ro',
-  isa       => ArrayObj,
+  isa       => TypedArray[ (Str | RegexpRef) ],
   coerce    => 1,
   builder   => sub { [] },
   handles   => +{
+    add_mask   => 'push',
     list_masks => 'all',
   },
 );
 
-sub matches {
+sub has_match {
   my ($self, $addr) = @_;
-  confess "Expected an address" unless defined $addr;
+  confess "Expected an address" unless defined $addr and length $addr;
   $self->masks->has_any(sub { 
-    ref $_ eq 'Regexp' ? $addr =~ $_ : _match($_, $addr)
+    ref eq 'Regexp' ? $addr =~ $_ : _match($_, $addr)
   })
-}
-
-sub add_mask {
-  my ($self, $mask) = @_;
-  confess 'Expected an address mask or a qr// Regexp object'
-    unless defined $mask and ref $mask eq 'Regexp' or not ref $mask;
-  $self->masks->push($mask);
 }
 
 sub del_mask {
@@ -61,3 +55,64 @@ sub del_mask {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+POEx::ZMQ::ZAP::AddressList - IP address whitelist/blacklist for POEx::ZMQ::ZAP
+
+=head1 SYNOPSIS
+
+FIXME
+
+=head1 DESCRIPTION
+
+FIXME
+
+=head2 Masks
+
+FIXME
+
+=head2 ATTRIBUTES
+
+=head3 masks
+
+A L<List::Objects::WithUtils::Array::Typed> containing either strings
+(glob-like masks) or C<Regexp>-type objects addresses will be matched against.
+
+=head2 METHODS
+
+=head3 has_match
+
+Takes an IP address; returns true if the address matches an entry present on
+the list.
+
+=head3 add_mask
+
+Adds IP addresses, masks, or regular expressions to the list for future
+matching via L</has_match>.
+
+Takes either a complete address, a glob-like mask (see L</Masks>), or a
+C<Regexp>-type object (typically produced via the C<qr//> operator).
+
+=head3 list_masks
+
+Returns the complete list of L</masks>; this is the same as calling 
+C<< $addrlist->masks->all >>.
+
+=head3 del_mask
+
+Removes IP addresses, masks, or regular expressions from the L</masks> list.
+
+Takes either a complete address, a glob-like mask (see L</Masks>), or a
+C<Regexp>-type object (typically produced via the C<qr//> operator).
+
+(If a C<Regexp>-type object is provided, stringy comparison is used; in other
+words, the exact object present on the list need not be used.)
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+=cut
