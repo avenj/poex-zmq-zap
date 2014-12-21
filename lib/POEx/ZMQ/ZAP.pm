1359ec22c3631b8ec5c275935fa4ccbbae5db5a8
Jon Portnoy <avenj@cobaltirc.org>
2014-12-21 18:12:41 -0500

    ZAP.pm outline

--- lib/POEx/ZMQ/ZAP.pm
+++ lib/POEx/ZMQ/ZAP.pm
@@ -1,6 +1,33 @@
 package POEx::ZMQ::ZAP;
 
-# FIXME
+use strictures 1;
+use Carp;
+use Scalar::Util 'blessed';
+
+use Moo::Role ();
+
+
+use Exporter::Tiny;
+our @EXPORT = our @EXPORT_OK = qw/
+  add_zap_options
+  create_zap_handler
+/;
+
+
+sub add_zap_options {
+  my ($zsock) = @_;
+  confess "Expected a POEx::ZMQ::Socket but got $zsock"
+    unless blessed $zsock and $zsock->isa('POEx::ZMQ::Socket');
+  Moo::Role->apply_roles_to_object( $zsock,
+    'POEx::ZMQ::ZAP::Role::SocketOptions'
+  );
+  $zsock
+}
+
+sub create_zap_handler {
+  # FIXME
+}
+
 
 print
   qq[<Capn_Refsmmat> rofer: who needs ethics committees? ],
